e9f54088ae0b82d397c8bb6bd6ad7ca609a0fa3f
ajit0507 <62989333+ajit0507@users.noreply.github.com>
2024-01-26 14:31:49 +0000

    code changes for string demo

---
+++ 01_basic/01_typeconversion.js
@@ -0,0 +1,39 @@
+let isLogged = true
+
+//console.log(isLogged)
+
+isLogged = Number(isLogged)
+
+//console.log( isLogged);
+
+//let mark = Number("32abc")
+//console.log(mark);
+
+let total = "100AC"
+let totalMark = Boolean(total)
+
+//console.log(totalMark);
+
+
+let student = {
+    name : "ajit",
+    email : "ajit@gmail.com"
+}
+
+student1 = student;
+
+student1.email = "ajit123@gmail.com"
+
+//console.log(student1.email);
+//console.log(student.email);
+
+
+let value = 10
+value1 =value
+
+value1 =20
+
+console.log(value);
+console.log(value1);
+
+
---
+++ 01_basic/01_typeofcheck.js
@@ -0,0 +1,27 @@
+let name = "ajit"
+
+//console.log(typeof name);
+let mark =10
+//console.log(typeof mark);
+
+let getMark = function() {
+    console.log(100);
+}
+
+//console.log(typeof getMark);
+
+let phone = null;
+
+//console.log( typeof phone)
+
+let email = undefined
+
+console.log(email);
+
+let student = {
+    email : "ajitpatil@gmail.com",
+    phone : "887788998"
+}
+
+console.log(typeof student);
+
