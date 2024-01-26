94c9f03dc836f23e6445ab4a133b0e76b916217a
ajit0507 <62989333+ajit0507@users.noreply.github.com>
2024-01-26 14:46:40 +0000

    strng demo

---
+++ 01_basic/01_stringdemo.js
@@ -0,0 +1,9 @@
+const myname = "ajit"
+
+const email = 'ajit@gmail.com'
+
+console.log(`my name is ${myname.toUpperCase()} and email id is ${email.toUpperCase()}`);
+
+
+const name = new String("ajit")
+
